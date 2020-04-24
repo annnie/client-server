83878d462ee5233101f3bb0cc19d3098ef53e82c
Ann Gerbeth <ann.gerbeth@resideo.com>
2020-04-23 22:28:35 -0400

    Added bootstrap templates, fixed carousel

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,10 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', indexRouter);
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('carousel', { title: 'Multi User Chat', name: 'ann' });
 });
 
 module.exports = router;
